3420d6d10d62e50acf0d27e6b5277545e6cdc8d7
Jamalperry <lee.perry400@gmail.com>
2022-12-20 10:57:19 -0400

    add slide show/hide toggle to scripts.js file and changed fadeToggle to slideToggle for all show/hide toggle code

--- js/scripts.js
+++ js/scripts.js
@@ -1,98 +1,104 @@
 $(document).ready(function() {
 
+  $(".clickerHeader").click(function() {
+    $(".shownHeader").slideToggle(1000, function() {
+      $(".hiddenHeader").slideToggle(1000);
+    });
+  });
+
   $(".clicker").click(function() {
-    $(".shown").fadeToggle(1000, function() {
-      $(".hidden").fadeToggle(1000);
+    $(".shown").slideToggle(1000, function() {
+      $(".hidden").slideToggle(1000);
     });
   });
 
   $(".clicker1").click(function() {
-    $(".shown1").fadeToggle(1000, function() {
-      $(".hidden1").fadeToggle(1000);
+    $(".shown1").slideToggle(1000, function() {
+      $(".hidden1").slideToggle(1000);
     });
   });
 
   $(".clicker2").click(function() {
-    $(".shown2").fadeToggle(1000, function() {
-      $(".hidden2").fadeToggle(1000);
+    $(".shown2").slideToggle(1000, function() {
+      $(".hidden2").slideToggle(1000);
     });
   });
 
   $(".clicker3").click(function() {
-    $(".shown3").fadeToggle(1000, function() {
-      $(".hidden3").fadeToggle(1000);
+    $(".shown3").slideToggle(1000, function() {
+      $(".hidden3").slideToggle(1000);
     });
   });
 
   $(".clicker4").click(function() {
-    $(".shown4").fadeToggle(1000, function() {
-      $(".hidden4").fadeToggle(1000);
+    $(".shown4").slideToggle(1000, function() {
+      $(".hidden4").slideToggle(1000);
     });
   });
 
   $(".clicker5").click(function() {
-    $(".shown5").fadeToggle(1000, function() {
-      $(".hidden5").fadeToggle(1000);
+    $(".shown5").slideToggle(1000, function() {
+      $(".hidden5").slideToggle(1000);
     });
   });
 
   $(".clicker6").click(function() {
-    $(".shown6").fadeToggle(1000, function() {
-      $(".hidden6").fadeToggle(1000);
+    $(".shown6").slideToggle(1000, function() {
+      $(".hidden6").slideToggle(1000);
     });
   });
 
   $(".clicker7").click(function() {
-    $(".shown7").fadeToggle(1000, function() {
-      $(".hidden7").fadeToggle(1000);
+    $(".shown7").slideToggle(1000, function() {
+      $(".hidden7").slideToggle(1000);
     });
   });
 
   $(".clicker8").click(function() {
-    $(".shown8").fadeToggle(1000, function() {
-      $(".hidden8").fadeToggle(1000);
+    $(".shown8").slideToggle(1000, function() {
+      $(".hidden8").slideToggle(1000);
     });
   });
 
   $(".clicker9").click(function() {
-    $(".shown9").fadeToggle(1000, function() {
-      $(".hidden9").fadeToggle(1000);
+    $(".shown9").slideToggle(1000, function() {
+      $(".hidden9").slideToggle(1000);
     });
   });
 
   $(".clicker10").click(function() {
-    $(".shown10").fadeToggle(1000, function() {
-      $(".hidden10").fadeToggle(1000);
+    $(".shown10").slideToggle(1000, function() {
+      $(".hidden10").slideToggle(1000);
     });
   });
 
   $(".clicker11").click(function() {
-    $(".shown11").fadeToggle(1000, function() {
-      $(".hidden11").fadeToggle(1000);
+    $(".shown11").slideToggle(1000, function() {
+      $(".hidden11").slideToggle(1000);
     });
   });
 
   $(".clicker12").click(function() {
-    $(".shown12").fadeToggle(1000, function() {
-      $(".hidden12").fadeToggle(1000);
+    $(".shown12").slideToggle(1000, function() {
+      $(".hidden12").slideToggle(1000);
     });
   });
 
   $(".clicker13").click(function() {
-    $(".shown13").fadeToggle(1000, function() {
-      $(".hidden13").fadeToggle(1000);
+    $(".shown13").slideToggle(1000, function() {
+      $(".hidden13").slideToggle(1000);
     });
   });
 
   $(".clicker14").click(function() {
-    $(".shown14").fadeToggle(1000, function() {
-      $(".hidden14").fadeToggle(1000);
+    $(".shown14").slideToggle(1000, function() {
+      $(".hidden14").slideToggle(1000);
     });
   });
 
   $(".clicker15").click(function() {
-    $(".shown15").fadeToggle(1000, function() {
-      $(".hidden15").fadeToggle(1000);
+    $(".shown15").slideToggle(1000, function() {
+      $(".hidden15").slideToggle(1000);
     });
   });
 
